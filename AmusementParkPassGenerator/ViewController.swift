2bc1e98b4b3e258f47c6c5d9255ae45de2431c65
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-08-08 23:22:24 -0700

    Add test cases

--- AmusementParkPassGenerator/ViewController.swift
+++ AmusementParkPassGenerator/ViewController.swift
@@ -9,13 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let hourlyEmployee = Manager(firstName: "Khalid", lastName: "Alrashed", streetAddress: "123", city: "SF", state: "CA", zipCode: 123)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swipe(entrant: hourlyEmployee, accessType: .maintenance)
+        do {
+            // Test 1
+//            let employee1Name = try FullName(firstName: "", lastName: "")
+//            let employee1Address = try FullAddress(streetAddress: "123", city: "SF", state: "CA", zipCode: 1234)
+//            let employee1 = Manager(fullName: employee1Name, fullAddress: employee1Address)
+//            swipe(entrant: employee1, accessType: .maintenance)
+            
+            // Test 2
+//            let employee2Name = try FullName(firstName: "", lastName: "")
+//            let employee2Address = try FullAddress(streetAddress: "123", city: "SF", state: "CA", zipCode: 1234)
+//            let employee2 = FoodServicesEmployee(fullName: employee2Name, fullAddress: employee2Address)
+//            swipe(entrant: employee2, accessType: .office)
+            
+            // Test 3
+//            let freeChildGuest1 = try FreeChildGuest(month: 4, day: 9, year: 2005)
+//            swipe(entrant: freeChildGuest1, accessType: .park)
+            
+            // Test 4
+//            let freeChildGuest2 = try FreeChildGuest(month: 4, day: 9, year: 2016)
+//            swipe(entrant: freeChildGuest2, accessType: .park)
+            
+        } catch {
+            print("Error: \(error)")
+        }
+        
+        // Test 5
+//        let classicGuest1 = ClassicGuest()
+//        swipe(entrant: classicGuest1, accessType: .foodDiscount)
     }
     
     func swipe(entrant: Entrant, accessType: AccessType) {
