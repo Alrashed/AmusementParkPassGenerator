4707fe5397632beba37a02ece5e90c66fb8984d5
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-08-16 16:28:16 -0700

    Hook up outlets and action buttons to view controller

--- AmusementParkPassGenerator/GeneratePassViewController.swift
+++ AmusementParkPassGenerator/GeneratePassViewController.swift
@@ -9,33 +9,59 @@
 import UIKit
 
 class GeneratePassViewController: UIViewController {
-
+    @IBOutlet weak var guestCategoryButton: UIButton!
+    @IBOutlet weak var employeeCategoryButton: UIButton!
+    @IBOutlet weak var managerCategoryButton: UIButton!
+    @IBOutlet weak var contractorCategoryButton: UIButton!
+    @IBOutlet weak var vendorCategoryButton: UIButton!
+    
+    @IBOutlet weak var subCategoryButton0: UIButton!
+    @IBOutlet weak var subCategoryButton1: UIButton!
+    @IBOutlet weak var subCategoryButton2: UIButton!
+    @IBOutlet weak var subCategoryButton3: UIButton!
+    @IBOutlet weak var subCategoryButton4: UIButton!
+    
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var projectTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var companyTextField: UITextField!
+    @IBOutlet weak var streetAddressTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Test 1
-        //            let employee1Name = try FullName(firstName: "", lastName: "")
-        //            let employee1Address = try FullAddress(streetAddress: "123", city: "SF", state: "CA", zipCode: 1234)
-        //            let employee1 = Manager(fullName: employee1Name, fullAddress: employee1Address)
-        //            swipe(entrant: employee1, accessType: .maintenance)
-        
-        // Test 2
-        //            let employee2Name = try FullName(firstName: "", lastName: "")
-        //            let employee2Address = try FullAddress(streetAddress: "123", city: "SF", state: "CA", zipCode: 1234)
-        //            let employee2 = FoodServicesEmployee(fullName: employee2Name, fullAddress: employee2Address)
-        //            swipe(entrant: employee2, accessType: .office)
-        
-        // Test 3
-        //            let freeChildGuest1 = try FreeChildGuest(month: 4, day: 9, year: 2005)
-        //            swipe(entrant: freeChildGuest1, accessType: .park)
-        
-        // Test 4
-        //            let freeChildGuest2 = try FreeChildGuest(month: 4, day: 9, year: 2016)
-        //            swipe(entrant: freeChildGuest2, accessType: .park)
-        
-        // Test 5
-        //        let classicGuest1 = ClassicGuest()
-        //        swipe(entrant: classicGuest1, accessType: .foodDiscount)
     }
+    
+    @IBAction func guestCategorySelected(_ sender: UIButton) {
+    }
+    
+    @IBAction func employeeCategorySelected(_ sender: UIButton) {
+    }
+    
+    @IBAction func managerCategorySelected(_ sender: UIButton) {
+    }
+    
+    @IBAction func contractorCategorySelected(_ sender: UIButton) {
+    }
+    
+    @IBAction func vendorCategorySelected(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func subCategorySelected(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func generatePass() {
+    }
+    
+    @IBAction func populateData() {
+    }
+    
 }
 
