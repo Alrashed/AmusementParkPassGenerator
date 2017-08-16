42b9e1b14dc0936bdd37623ca3cca28e0f25f589
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-08-16 14:10:42 -0700

    Create and design UI for GeneratePassViewController

--- AmusementParkPassGenerator/GeneratePassViewController.swift
+++ AmusementParkPassGenerator/GeneratePassViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GeneratePassViewController.swift
 //  AmusementParkPassGenerator
 //
 //  Created by Khalid Alrashed on 8/7/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GeneratePassViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
