c7411e123b83438e97367c284d10fa8b167a97f5
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-08-17 22:11:30 -0700

    Hook up outlets and buttons to test pass view controller

---
+++ AmusementParkPassGenerator/TestPassViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TestPassViewController.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Khalid Alrashed on 8/17/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class TestPassViewController: UIViewController {
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var passDescriptionLabel: UILabel!
+    @IBOutlet weak var rideDescriptionLabel: UILabel!
+    @IBOutlet weak var foodDiscountLabel: UILabel!
+    @IBOutlet weak var merchDiscountLabel: UILabel!
+    @IBOutlet weak var testResultsLabel: UILabel!
+    
+    @IBOutlet weak var testAccessButton0: UIButton!
+    @IBOutlet weak var testAccessButton1: UIButton!
+    @IBOutlet weak var testAccessButton2: UIButton!
+    @IBOutlet weak var testAccessButton3: UIButton!
+    @IBOutlet weak var testAccessButton4: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func createNewPass(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func showAreaAccessTests(_ sender: UIButton) {
+    }
+    
+    @IBAction func showRideAccessTests(_ sender: UIButton) {
+    }
+    
+    @IBAction func showDiscountAccessTests(_ sender: UIButton) {
+    }
+    
+    @IBAction func accessTestSelected(_ sender: UIButton) {
+    }
+}
